b6a678924461cf190ea5edb916c2d202f526ec00
misa <migocr@gmail.com>
2020-10-28 12:05:16 -0700

    Simbologia corregida, prueba de constantes (const)

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var nameuser "john";
-let lastname "carter";
+var nameuser = "john";
+let lastname = "carter";
 
 nameuser = 'pepe';
 
 console.log(nameuser);
+
+//las constantes a diferencia de las variables no cambian.
+const PI=3.1416;
