41ce9d4242c5631cdd4144448cd4895f82c04e33
misa <migocr@gmail.com>
2020-10-28 12:21:26 -0700

    comparacion entre var y let y comentarios descriptivos agregados

--- app.js
+++ app.js
@@ -12,11 +12,19 @@ const PI=3.1416;
 
 //prueba con variables let
 
-let x = 1;
+let x = 1; //x vale 1
 if (true) {
-    let x = 2;
+    let x = 2; //x dentro de esta funcion vale 2
     console.log(x);
     //nos imprimira el valor de x que en este bloque es 2
 }
-console.log(x);
-//x aqui vale 1 nuevamente, pues esta fuera de la funcion anterior y su valor esta definido.
+console.log(x); //x aqui vale 1 nuevamente, pues esta fuera de la funcion anterior y su valor esta definido.
+
+//en el caso de variables var, el valor se continua guardando incluso fuera del bloque
+
+var x = 1; //x vale 1
+if (true) {
+    var x = 2; //x ahora vale 2
+    console.log(x); 
+}
+console.log(x); //x sigue valiendo 2
